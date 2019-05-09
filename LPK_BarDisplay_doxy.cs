839308ec57d12ca3a0be23532d996665ccdfe63c
Aeon <aeonwilliams@gmail.com>
2019-05-09 13:24:08 -0700

    Alpha Working State
    Project does everything except for replace Chris' comments with Doxygen comments.

---
+++ LPK_BarDisplay_doxy.cs
@@ -0,0 +1,58 @@
+﻿/***************************************************
+File:           LPK_BarDisplay.cs
+Authors:        Christopher Onorati
+Last Updated:   12/9/2018
+Last Version:   2.17
+
+Description:
+  This component controls the appearance of a display bar 
+  such as a health or cooldown bar.
+
+This script is a basic and generic implementation of its 
+functionality. It is designed for educational purposes and 
+aimed at helping beginners.
+
+Copyright 2018-2019, DigiPen Institute of Technology
+***************************************************/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;   //Access to Slider
+
+/**
+* CLASS NAME  : LPK_BarDisplay
+* DESCRIPTION : Create a display using a slider in the UI canvas.
+**/
+[RequireComponent(typeof(Slider))]
+public class LPK_BarDisplay : LPK_LogicBase
+{
+    /**
+    * FUNCTION NAME: OnStart
+    * DESCRIPTION  : Initializes events.
+    * INPUTS       : None
+    * OUTPUTS      : None
+    **/
+    override protected void OnStart()
+    {
+        LPK_EventManager.OnLPK_DisplayUpdate += OnEvent;
+    }
+
+    /**
+    * FUNCTION NAME: OnEvent
+    * DESCRIPTION  : Changes the visible display of the bar.
+    * INPUTS       : data - Event data to parse for validation.
+    * OUTPUTS      : None
+    **/
+    override protected void OnEvent(LPK_EventManager.LPK_EventData data)
+    {
+        //Incorrect object.
+        if (!ShouldRespondToEvent(data))
+            return;
+
+        if (m_bPrintDebug)
+            LPK_PrintDebug(this, "Display Update");
+
+        GetComponent<Slider>().value = data.m_flData[0] / data.m_flData[1];
+    }
+}
--- christodoxygen.cpp
+++ christodoxygen.cpp
@@ -2,8 +2,9 @@
 #include <string>
 #include <experimental/filesystem> 
 #include <list>
-#include <iostream>
 #include <fstream>
+#include <locale>
+#include <windows.h>
 using namespace std;
 namespace fs = experimental::filesystem;
 
@@ -47,12 +48,13 @@ int main(void)
     string nameOfCurrFile;
     string metaKey = ".meta";
     string csKey = ".cs";
+    string lpkDirectory = "C:/Users/Nicholas/Documents/Aeon Work/TestDir/";
 
     /**
      * Scans a given directory and store each .cs file into a linked list
     **/
     //directory of the files to scan
-    string path = "C:/Users/Nicholas/Unity LPK/Assets/Scripts/LPK";
+    string path = lpkDirectory;
     //scan the directory, file by file
     for(const auto & entry : fs::directory_iterator(path))
     {
@@ -69,14 +71,43 @@ int main(void)
         }
     }
 
-    /*
-    Var definitions for shit
-    */
+    /**
+     * For each .cs file in the created list, make a copy, and edit the syntax of Chris' comments to be doxygen style comments
+    **/
+   ifstream currFile;
+   string line;
+   //loop through the list and edit each file
    for(int i = 0; i < listOfFiles.size(); i++)
-   {
+   {  
        string fileName = accessItem(listOfFiles, i);
+       string filePath = lpkDirectory + fileName;
+       string tempFileName = (fileName.substr(0,fileName.size() -3)) + "_temp.cs" ;
+       string doxyFileName = (fileName.substr(0,fileName.size() - 3)) + "_doxy.cs";
+
+       /*cout << fileName << endl;
+       cout << filePath << endl;
+       cout << tempFileName << endl;
+       cout << doxyFileName << endl;
+       cout << "\n";*/
+
+       CopyFile(filePath.c_str(), tempFileName.c_str(), TRUE);
+       currFile.open(tempFileName.c_str());
+
+       ofstream doxyFile;
+       remove(doxyFileName.c_str());
+       doxyFile.open(doxyFileName.c_str());
+
+       if(currFile.is_open())
+       {
+           while(getline (currFile, line))
+           {
+               doxyFile << line << endl;
+           }
+       }
 
 
+       //delete the temp file at the end
+       remove(tempFileName.c_str());
    }
 
 
